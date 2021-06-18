be6d526e3a5082bbac6177f832eec3fed7262043
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-06-19 01:17:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pytz
 import pyrogram
 
 # Api Strings From my.telegram.org
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("API_ID", 3706754))
 API_HASH = os.environ.get("API_HASH")
 
 # Your Session Strings
